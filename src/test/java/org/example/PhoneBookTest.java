43a41fccb1d20fe57ef2054d652f5f89c520e313
RinatG <rinat_galimov_1992@mail.ru>
2023-11-20 23:04:50 +0300

    Stub and tests for add

--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import org.junit.Assert;
+
 class PhoneBookTest {
+    PhoneBook phoneBook = new PhoneBook();
+    void addTest(){
+        int answer = phoneBook.add("Rin","+79164381345");
+        Assert.assertEquals(1,answer);
+    }
 
 }
